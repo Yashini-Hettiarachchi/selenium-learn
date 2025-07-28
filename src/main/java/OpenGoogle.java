55d807094b61cc5d915530e594dfbc861b236dc1
yashini-hettiarachchi <yashinihettiarachchi99@gmail.com>
2025-07-28 14:58:14 +0530

    using Test folder

---
+++ src/main/java/OpenGoogle.java
@@ -0,0 +1,23 @@
+//import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenGoogle {
+    //Open google
+    //Go to Google home page
+    public static void main(String[] args) {
+//        System.setProperty("chrome",""); for normal human
+
+//        System.setProperty("webdriver.chrome.driver","D:\\selenium\\driver\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+//        WebDriverManager.chromedriver().setup();
+//        for selenium
+        WebDriver driver=new ChromeDriver(); //WebDriver-> major component
+        driver.get("https://www.google.com/");
+        driver.quit(); //exit code 0 successfully run
+//                driver.close()
+
+
+
+
+    }
+}
--- src/test/java/OpenGoogleTest.java
+++ src/test/java/OpenGoogleTest.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class OpenGoogleTest {
+    @Test
+    public void GoogleTest() {
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.quit();
+    }
 }
